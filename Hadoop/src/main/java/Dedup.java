b3cc5f50d12df9f3aa9304537ea457894acb95f8
TW <tanwen19932@163.com>
2016-10-13 20:58:43 +0800

    segmentation of Hadoop and Spark

--- Hadoop/src/main/java/Dedup.java
+++ Hadoop/src/main/java/Dedup.java
@@ -1,19 +1,11 @@
 import org.apache.hadoop.conf.Configuration;
-
 import org.apache.hadoop.fs.Path;
-
 import org.apache.hadoop.io.Text;
-
 import org.apache.hadoop.mapreduce.Job;
-
 import org.apache.hadoop.mapreduce.Mapper;
-
 import org.apache.hadoop.mapreduce.Reducer;
-
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 
 import java.io.File;
 import java.io.IOException;
